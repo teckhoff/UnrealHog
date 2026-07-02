550009407ca91aa66bd9091cbef901373f241038
Trevor Eckhoff <trevor@ceckhoff.net>
2026-07-02 17:53:28 -0400

    Initial commit

---
+++ Source/UnrealHog/Private/UnrealHog.cpp
@@ -0,0 +1,20 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "UnrealHog.h"
+
+#define LOCTEXT_NAMESPACE "FUnrealHogModule"
+
+void FUnrealHogModule::StartupModule()
+{
+	// This code will execute after your module is loaded into memory; the exact timing is specified in the .uplugin file per-module
+}
+
+void FUnrealHogModule::ShutdownModule()
+{
+	// This function may be called during shutdown to clean up your module.  For modules that support dynamic reloading,
+	// we call this function before unloading the module.
+}
+
+#undef LOCTEXT_NAMESPACE
+	
+IMPLEMENT_MODULE(FUnrealHogModule, UnrealHog)
